15a7293b30a7768845e3bfed33186159cd16df14
TheNuRii <maciekglow0@gmail.com>
2023-08-29 18:57:27 +0200

    Done with tasks from a7 to a11

--- activities/src/bin/a11.rs
+++ activities/src/bin/a11.rs
@@ -5,8 +5,25 @@
 //
 // Notes:
 // * Use a struct for the grocery item
+struct GroceryItem {
+    id: i32,
+    quantity: i32,
+}
 // * Use two i32 fields for the quantity and id number
 // * Create a function to display the quantity, with the struct as a parameter
 // * Create a function to display the id number, with the struct as a parameter
+fn dispaly_quantity(item: &GroceryItem) {
+    println!("quantity: {:?}", item.quantity);
+}
+fn dispaly_id(item: &GroceryItem) {
+    println!("id: {:?}", item.id);
+}
 
-fn main() {}
+fn main() {
+    let carrot = GroceryItem {
+        id: 1,
+        quantity: 10,
+    };
+    dispaly_quantity(&carrot);
+    dispaly_id(&carrot);
+}
--- activities/src/bin/a8.rs
+++ activities/src/bin/a8.rs
@@ -8,5 +8,32 @@
 // * Use a struct to store drink flavor and fluid ounce information
 // * Use a function to print out the drink flavor and ounces
 // * Use a match expression to print the drink flavor
+enum Flavor {
+    OrangeJuce,
+    AppleJuce,
+    Beer,
+    Water,
+}
 
-fn main() {}
+struct Drink {
+    drink_flavor: Flavor,
+    capacity_liters: f64,
+}
+
+fn print_drink (drink: Drink){
+    match drink.drink_flavor {
+        Flavor::AppleJuce => println!("flavor: AppleJuce"),
+        Flavor::Beer => println!("flavor: Beer"),
+        Flavor::Water => println!("flavor: Water"),
+        Flavor::OrangeJuce => println!("flavor: OrangeJuce"),
+    }
+    println!("liters: {:?}", drink.capacity_liters);
+}
+fn main() {
+    let favorite_drink = Drink {
+        drink_flavor: Flavor::Beer,
+        capacity_liters: 0.5,
+    };
+    print_drink(favorite_drink);
+}
+
